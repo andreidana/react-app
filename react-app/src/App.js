43ab345a3a75b764f182bf6e6afa8fea8b70ac3f
Andrei Dana <andrei.dana87@gmail.com>
2019-04-08 22:37:20 +0300

    Dynamically render names

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -12,19 +12,42 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
-    this.state.persons[0].Name = "Andrei Dana";
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        {name: newName, age: 31},
+        {name: 'Larisa', age: 29},
+        {name: 'Cristina', age: 1},
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: event.target.value, age: 31},
+        {name: 'Larisa', age: 29},
+        {name: 'Cristina', age: 1},
+      ]
+    });
   }
 
   render() {
     return (
       <div className="app">
         <h1>Hello World, React app!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}><h1>My Hobbies: Reading</h1></Person>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+        <button onClick={this.switchNameHandler.bind(this, 'TO-DO')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          changed={this.nameChangedHandler}/>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}/>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler.bind(this, 'WIP')}><h1>My Hobbies: Reading</h1></Person>
       </div>
     );
   }
